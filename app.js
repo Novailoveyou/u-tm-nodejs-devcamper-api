bedff1d2d8bbca8322d39929dad8436fd1f3f674
Novailoveyou <novailoveyoux@gmail.com>
2020-12-15 16:51:09 +0300

    created first model

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const dotenv = require('dotenv')
-// const logger = require('./middleware/logger')
 const morgan = require('morgan')
 const colors = require('colors')
 const connectDB = require('./config/db')
@@ -16,24 +15,10 @@ const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
-// app.use(logger)
-
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-
-// app.get('/', (req, res) => {
-//   // res.send('Hello World')
-//   // res.send('<h1>Hello World</h1>') // would work, since in Headers default Content-Type is text/html
-//   // res.send({ name: 'Nover' }) // Express knows that we're sending JSON even tho it's a JS object
-//   // res.json({ name: 'Nover' }) // Still better to use .json in case of sending JSON, epxress would format it to make it valid
-
-//   // Send status
-//   // res.sendStatus(400) // Better way below
-//   // res.status(400).json({ success: false, error: 'Artificial error' })
-//   res.status(200).json({ success: true, data: { id: 1 } }) // An example of what a successful response would look like
-// })
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
