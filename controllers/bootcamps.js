a930ad2c1f679695c1aa78254a11d6dc34bea2b2
Novailoveyou <novailoveyoux@gmail.com>
2020-12-16 16:40:28 +0300

    pagination added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query }
 
   // Fields to exclude
-  const removeFields = ['select', 'sort']
+  const removeFields = ['select', 'sort', 'page', 'limit']
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param])
@@ -44,11 +44,44 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt')
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+  const totalPages = Math.ceil(total / limit)
+
+  query = query.skip(startIndex).limit(limit)
+
   // Executing query
   const bootcamps = await query
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps })
+
+  // Pagination result
+  const pagination = {}
+
+  if (endIndex < total) {
+    pagination.limit = limit
+    pagination.totalPages = totalPages
+    pagination.next = {
+      page: page + 1,
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.limit = limit
+    pagination.totalPages = totalPages
+    pagination.prev = {
+      page: page - 1,
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  })
 })
 
 // @desc      Get single bootcamps
